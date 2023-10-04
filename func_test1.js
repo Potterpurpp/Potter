263b0b45ad7a748a64e2148baa931eabb67b5731
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-04 10:00:43 +0700

    Test case come

--- func_test1.js
+++ func_test1.js
@@ -1,15 +1,27 @@
-const { helloworld, one_plus } =require("");
-if(helloworld()==="hello world"){
+const { helloworld, one_plus } = require("./Utils");
 
-}
-else{
+if(helloworld() === "hello world")
+{
+
+}else{
     console.log(1);
     return;
 }
-if(one_plus === 1){
 
+//test case: one_plus 
+if(one_plus(1) === 2) //oneplus <--- 1 ==> 2
+{
+    
+}else{
+    console.log(1);
+    return;
 }
-else{
+
+
+if(one_plus(2) === 3) //oneplus <--- 1 ==> 2
+{
+    
+}else{
     console.log(1);
     return;
 }

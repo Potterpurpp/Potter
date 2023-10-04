c39bb1252eb77570e37e50a62d88cda969ddd426
Potterpurpp <118960067+Potterpurpp@users.noreply.github.com>
2023-10-04 09:52:48 +0700

    Hi

---
+++ test_function.js
@@ -0,0 +1,15 @@
+const { helloworld, one_plus } =require("");
+if(helloworld()==="hello world"){
+
+}
+else{
+    console.log(1);
+    return;
+}
+if(one_plus === 1){
+
+}
+else{
+    console.log(1);
+    return;
+}
